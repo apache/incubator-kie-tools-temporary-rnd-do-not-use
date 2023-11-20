18da264fbdc7cc8a542a5b29bc06aa18175e0adb
Rodrigo Antunes <contato@rodrigoantunes.com.br>
2023-11-20 08:37:59 -0300

    Add github codeql credential id var

--- .ci/jenkins/kie-tools-pipeline-lib/vars/pipelineVars.groovy
+++ .ci/jenkins/kie-tools-pipeline-lib/vars/pipelineVars.groovy
@@ -8,6 +8,7 @@ class pipelineVars implements Serializable {
     String openshiftCredentialsId = 'openshift-kie-tools-token'
     String kieToolsBotGithubCredentialsId = 'kie-tools-bot-gh'
     String kieToolsBotGithubTokenCredentialsId = 'kie-tools-bot-gh-token'
+    String kieToolsGithubCodeQLTokenCredentialsId = 'kie-tools-gh-codeql-token'
     String chromeStoreCredentialsId = 'kie-tools-chome-store'
     String chromeStoreRefreshTokenCredentialsId = 'kie-tools-chome-store-refresh-token'
     String npmTokenCredentialsId = 'kie-tools-npm-token'
