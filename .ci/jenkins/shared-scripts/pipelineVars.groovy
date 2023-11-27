79149ac2a563fb72bb9dac4693338b427517dc37
Rodrigo Antunes <contato@rodrigoantunes.com.br>
2023-11-27 09:22:03 -0300

    CI - Sync changes (#22)
    * CI - Disable workflows migrated to Jenkins (#20)
    
    * CI - Disable workflows migrated to Jenkins
    
    * Update CI Build workflow name
    
    * Remove disallowed actions
    
    * CI - Jenkins shared scripts (#21)
    
    * CI - Jenkins shared scripts
    
    * Update openshift get route utility
    
    * CI - Daily dev publish Jenkins job (#10)
    
    * CI - Daily dev publish Jenkins job
    
    * Fix artifacts and build date
    
    * Add env debug
    
    * remove env debug
    
    * Small code improvements
    
    * remove unecessary code
    
    * Start using the kie-tools-pipeline-lib utilities
    
    * Fix identation
    
    * Reflect library changes
    
    * Use shared scripts
    
    * Use shared scripts
    
    * Update image tag
    
    * Update Jenkinsfile name
    
    * Add DOCKER_CONFIG env var

---
+++ .ci/jenkins/shared-scripts/pipelineVars.groovy
@@ -0,0 +1,20 @@
+class PipelineVars implements Serializable {
+
+    String githubRepositoryOrg = 'apache';
+    String githubRepositoryName = 'incubator-kie-tools-temporary-rnd-do-not-use';
+    String githubRepositorySlug = 'apache/incubator-kie-tools-temporary-rnd-do-not-use';
+
+    String quayPushCredentialsId = 'quay-io-kie-tools-token';
+    String openshiftCredentialsId = 'openshift-kie-tools-token';
+    String kieToolsBotGithubCredentialsId = 'kie-tools-bot-gh';
+    String kieToolsBotGithubTokenCredentialsId = 'kie-tools-bot-gh-token';
+    String kieToolsGithubCodeQLTokenCredentialsId = 'kie-tools-gh-codeql-token';
+    String chromeStoreCredentialsId = 'kie-tools-chome-store';
+    String chromeStoreRefreshTokenCredentialsId = 'kie-tools-chome-store-refresh-token';
+    String npmTokenCredentialsId = 'kie-tools-npm-token';
+
+    String defaultArtifactsTempDir = 'artifacts-tmp';
+
+}
+
+return new PipelineVars();
