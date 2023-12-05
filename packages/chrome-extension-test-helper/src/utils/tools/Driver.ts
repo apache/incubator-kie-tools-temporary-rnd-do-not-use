d2f260c2dedf67ff7ceb326f284e4532df9c64d4
Rodrigo Antunes <contato@rodrigoantunes.com.br>
2023-12-05 17:51:27 -0300

    More chrome options

--- packages/chrome-extension-test-helper/src/utils/tools/Driver.ts
+++ packages/chrome-extension-test-helper/src/utils/tools/Driver.ts
@@ -44,6 +44,10 @@ export default class Driver {
     // init chrome options
     const chromeOptions: Options = new Options();
     chromeOptions.addArguments(
+      "--no-sandbox",
+      "--headless",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
       "--load-extension=" + chromeExtensionPath,
       "--enable-features=UnexpireFlagsM118",
       "--allow-insecure-localhost",
